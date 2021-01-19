dc5f55659aa97167dd3e96acd57b724565cdd201
jirubizu <zilvysdovydas@yahoo.co.jp>
2021-01-19 01:39:43 +0000

    Implemented UserJoined + Left events

--- Handlers/Events/UserJoinedHandler.cs
+++ Handlers/Events/UserJoinedHandler.cs
@@ -1,7 +1,59 @@
-﻿namespace Auditor.Handlers.Events
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Auditor.Services;
+using Auditor.Structures;
+using Discord;
+using Discord.Rest;
+using Discord.WebSocket;
+using Serilog;
+
+namespace Auditor.Handlers.Events
 {
     public class UserJoinedHandler : EventHandler
     {
+        private readonly DatabaseService database;
+        private readonly DiscordShardedClient shard;
+        private readonly ILogger logger = Log.ForContext<UserJoinedHandler>();
 
+        public UserJoinedHandler(DiscordShardedClient s, DatabaseService d)
+        {
+            this.shard = s;
+            this.database = d;
+            this.logger.Information("Registered");
+            this.shard.UserJoined += ShardOnUserJoined;
+        }
+
+        private async Task ShardOnUserJoined(SocketGuildUser user)
+        {
+            GuildBson guild = await this.database.LoadRecordsByGuildId(user.Guild.Id);
+
+            if (GetRestTextChannel(this.shard, guild.UserJoinedEvent.Key, out RestTextChannel restTextChannel))
+            {
+                List<EmbedFieldBuilder> fields = new()
+                {
+                    new EmbedFieldBuilder
+                    {
+                        Name = "Username",
+                        Value = user.Username
+                    },
+                    new EmbedFieldBuilder
+                    {
+                        Name = "User Id",
+                        Value = user.Id
+                    }
+                };
+
+                EmbedBuilder embedBuilder = new()
+                {
+                    Color = Color.Blue,
+                    Fields = fields,
+                    ImageUrl = user.GetAvatarUrl(),
+                    Footer = new EmbedFooterBuilder {Text = "Joined on " + DateTime.Now}
+                };
+                
+                await restTextChannel.SendMessageAsync("", false, embedBuilder.Build());
+            }
+        }
     }
 }
