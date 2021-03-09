006532d8f55607e2fe7fe7820a34e614b6a7429f
jirubizu <zilvysdovydas@yahoo.co.jp>
2021-03-09 04:29:08 +0000

    Implemented MessageBulkDeleteHandler

--- Handlers/Events/MessagesBulkDeletedHandler.cs
+++ Handlers/Events/MessagesBulkDeletedHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Auditor.Services;
 using Discord;
@@ -11,19 +12,41 @@ namespace Auditor.Handlers.Events
     {
         private readonly DatabaseService database;
         private readonly DiscordShardedClient shard;
+        private readonly PaginationService paginationService;
         private readonly ILogger logger = Log.ForContext<MessagesBulkDeletedHandler>();
 
-        public MessagesBulkDeletedHandler(DiscordShardedClient s, DatabaseService d)
+
+        public MessagesBulkDeletedHandler(DiscordShardedClient s, DatabaseService d, PaginationService p)
         {
             this.database = d;
             this.shard = s;
+            this.paginationService = p;
             this.shard.MessagesBulkDeleted += ShardOnMessagesBulkDeleted;
             logger.Information("Registered");
         }
 
-        private Task ShardOnMessagesBulkDeleted(IReadOnlyCollection<Cacheable<IMessage, ulong>> messages, ISocketMessageChannel textChannel)
+        private async Task ShardOnMessagesBulkDeleted(IReadOnlyCollection<Cacheable<IMessage, ulong>> cachedMessages,
+            ISocketMessageChannel restTextChannel)
         {
-            throw new System.NotImplementedException();
+            List<EmbedBuilder> pages = new();
+            foreach (Cacheable<IMessage, ulong> message in cachedMessages)
+            {
+                pages.Add(new EmbedBuilder
+                {
+                    Author = new EmbedAuthorBuilder
+                    {
+                        Name = message.Value.Author.Mention, IconUrl = message.Value.Author.GetAvatarUrl()
+                    },
+                    Description = message.Value.Content,
+                    Footer = new EmbedFooterBuilder
+                    {
+                        Text = $"Author ID: {message.Value.Author.Id}, at {DateTime.UtcNow} UTC"
+                    }
+                });
+            }
+
+            PaginatedMessage paginatedMessage = new(pages, "Bulk Delete", Color.Blue);
+            await this.paginationService.SendMessageAsync(restTextChannel, paginatedMessage);
         }
     }
 }
